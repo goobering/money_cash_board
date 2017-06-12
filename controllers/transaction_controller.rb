25bc7e981b162774415ba9d8d080443542c22032
goobering <goobering@gmail.com>
2017-06-12 13:34:53 +0100

    Added form to handle searching by tag to transaction_controller.erb. Added corresponding route. Created transactions/search_by_tag.erb to display result.

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -11,10 +11,18 @@ get '/person/:id/transactions' do
   @person = Person.find(params[:id].to_i)
   @transaction_value = Transaction.sum_values(@person.transactions())
   @transactions = Transaction.all_by_person(@person)
+  @tags = Tag.all()
   erb(:'transactions/index')
 end
 
-def get_tag_name(id)
-  tag = Tag.find(id)
-  return tag.name
+post '/person/:id/searchbytag/' do
+  redirect to("/person/#{params[:id]}/transactions/searchbytag/#{params[:tag_id]}")
+end
+
+get '/person/:id/transactions/searchbytag/:tag_id' do
+  @person = Person.find(params[:id].to_i)
+  @tag = Tag.find(params[:tag_id].to_i)
+  @transactions = Transaction.all_for_person_by_tag(@person, @tag)
+  @transaction_value = Transaction.sum_values(@transactions)
+  erb(:'transactions/search_by_tag')
 end

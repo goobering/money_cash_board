b425b9a7ce41a05f77c7d05c62cfe3d33e6b4233
goobering <goobering@gmail.com>
2017-06-12 14:22:07 +0100

    Created create_transaction.erb and new_transaction.erb view files and populated appropriately. Added 'Create new transaction' route to transaction_controller.rb. Complted search_by_Tag.erb.

--- controllers/transaction_controller.rb
+++ controllers/transaction_controller.rb
@@ -7,6 +7,7 @@ require_relative '../models/tag.rb'
 
 require 'pry-byebug'
 
+# Show list of transactions belonging to given user
 get '/person/:id/transactions' do
   @person = Person.find(params[:id].to_i)
   @transaction_value = Transaction.sum_values(@person.transactions())
@@ -15,14 +16,30 @@ get '/person/:id/transactions' do
   erb(:'transactions/index')
 end
 
+# Accept tag value selected by user, redirect to appropriate page of transactions
 post '/person/:id/searchbytag/' do
   redirect to("/person/#{params[:id]}/transactions/searchbytag/#{params[:tag_id]}")
 end
 
+# Display transactions belonging to a given user, with a given tag
 get '/person/:id/transactions/searchbytag/:tag_id' do
   @person = Person.find(params[:id].to_i)
   @tag = Tag.find(params[:tag_id].to_i)
   @transactions = Transaction.all_for_person_by_tag(@person, @tag)
   @transaction_value = Transaction.sum_values(@transactions)
+  @tags = Tag.all()
   erb(:'transactions/search_by_tag')
+end
+
+get '/person/:id/transactions/new' do
+  @person = Person.find(params[:id].to_i)
+  @tags = Tag.all()
+  erb(:'transactions/new_transaction')
+end
+
+post '/person/:person_id/transactions' do
+  # binding.pry
+  @transaction = Transaction.new(params)
+  @transaction.save()
+  erb(:'transactions/create_transaction')
 end

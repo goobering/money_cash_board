268939728484e0fc50e709d245e38883c6a348b0
goobering <goobering@gmail.com>
2017-06-13 11:25:44 +0100

    Added enormous list of seed Tag values to seeds.rb. Added update() and delete() methods to tag.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,67 @@ person_2.save()
 person_3 = Person.new({'first_name' => 'Frieda', 'last_name' => 'McFrieda'})
 person_3.save()
 
-tag_1 = Tag.new({'name' => 'Food'})
-tag_1.save()
-tag_2 = Tag.new({'name' => 'Travel'})
-tag_2.save()
-tag_3 = Tag.new({'name' => 'Pets'})
-tag_3.save()
+tag_array = [
+  {'name' => 'Arts'},
+  {'name' => 'Music'},
+  {'name' => 'Movies & DVDs'},
+  {'name' => 'Newspaper'},
+  {'name' => 'Tuition'},
+  {'name' => 'Student Loan'},
+  {'name' => 'Books & Supplies'},
+  {'name' => 'Clothing'},
+  {'name' => 'Books'},
+  {'name' => 'Electronics & Software'},
+  {'name' => 'Hobbies'},
+  {'name' => 'Sporting Goods'},
+  {'name' => 'Laundry'},
+  {'name' => 'Hair'},
+  {'name' => 'Spa & Massage'},
+  {'name' => 'Dentist'},
+  {'name' => 'Doctor'},
+  {'name' => 'Eyecare'},
+  {'name' => 'Pharmacy'},
+  {'name' => 'Health Insurance'},
+  {'name' => 'Gym'},
+  {'name' => 'Sports'},
+  {'name' => 'Kids Activites'},
+  {'name' => 'Kids Allowance'},
+  {'name' => 'Baby Supplies'},
+  {'name' => 'Babysitter'},
+  {'name' => 'Child Support'},
+  {'name' => 'Toys'},
+  {'name' => 'Groceries'},
+  {'name' => 'Coffee shops'},
+  {'name' => 'Fast Food'},
+  {'name' => 'Restaurants'},
+  {'name' => 'Alchohol & Bars'},
+  {'name' => 'Gift'},
+  {'name' => 'Charity'},
+  {'name' => 'Television'},
+  {'name' => 'Home Phone'},
+  {'name' => 'Internet'},
+  {'name' => 'Mobile Phone'},
+  {'name' => 'Utilities'},
+  {'name' => 'Gas & Fuel'},
+  {'name' => 'Parking'},
+  {'name' => 'Service & Auto Parts'},
+  {'name' => 'Auto Payment'},
+  {'name' => 'Auto Insurance'},
+  {'name' => 'Air Travel'},
+  {'name' => 'Rental Car & Taxi'},
+  {'name' => 'Vacation'}
+]
 
-transaction_1 = Transaction.new({'merchant_name' => 'Tesco', 'value' => 20, 'person_id' => person_1.id, 'tag_id' => tag_1.id})
+for tag in tag_array 
+  new_tag = Tag.new(tag)
+  new_tag.save()
+end
+
+transaction_1 = Transaction.new({'merchant_name' => 'Tesco', 'value' => 20, 'person_id' => person_1.id, 'tag_id' => Tag.all()[0].id})
 transaction_1.save()
-transaction_2 = Transaction.new({'merchant_name' => 'B & Q', 'value' => 30, 'person_id' => person_2.id, 'tag_id' => tag_2.id})
+transaction_2 = Transaction.new({'merchant_name' => 'B & Q', 'value' => 30, 'person_id' => person_2.id, 'tag_id' => Tag.all()[1].id})
 transaction_2.save()
-transaction_3 = Transaction.new({'merchant_name' => 'Netflix', 'value' => 40, 'person_id' => person_3.id, 'tag_id' => tag_3.id})
+transaction_3 = Transaction.new({'merchant_name' => 'Netflix', 'value' => 40, 'person_id' => person_3.id, 'tag_id' => Tag.all()[2].id})
 transaction_3.save()
 
 binding.pry
--- models/tag.rb
+++ models/tag.rb
@@ -16,17 +16,27 @@ class Tag
     @id = result[0]['id'].to_i
   end
 
-  def self.all()
-    sql = "SELECT * FROM tags;"
-    result = SqlRunner.run(sql)
-    return result.map { |tag| Tag.new(tag)}
-  end
-
   def find()
     sql = "SELECT * FROM tags WHERE tags.id = #{@id};"
     result = SqlRunner.run(sql)
     return Tag.new(result[0])
   end
+
+  def update(options)
+      sql = "UPDATE tags SET (name) = ('#{options['name']}' WHERE id = #{options['id']};"
+      SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM tags WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tags;"
+    result = SqlRunner.run(sql)
+    return result.map { |tag| Tag.new(tag)}
+  end  
 
   def self.find(id)
     sql = "SELECT * FROM tags WHERE tags.id = #{id};"
